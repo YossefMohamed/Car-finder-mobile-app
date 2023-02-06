ec87e093b422f760b566668fe9828a6e263b9a1a
YossefMohamed <yossefmohamed112233@gmail.com>
2023-02-06 11:11:58 +0200

    add initaial UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, Text, View, StatusBar } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Provider as PaperProvider, Searchbar } from "react-native-paper";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const onChangeSearch = (text: string) => setSearchQuery(text);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "purple" }}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "violet" }}>
+          <Text>Content</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
