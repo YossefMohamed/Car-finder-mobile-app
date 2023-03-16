a61e84ce11bae6f8d4cae6668b8d07175b3d1275
Yossef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2023-03-16 11:33:41 +0200

    update create car controller and multer middleware

--- server/src/controllers/carControllers/createCar.ts
+++ server/src/controllers/carControllers/createCar.ts
@@ -7,42 +7,45 @@ export const createCar = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { title, description, price, categoryId } = req.body as ICar;
+  try {
+    const { title, description, price, categoryId } = req.body as ICar;
+    if (!req.files) return next(new Error("Kindly upload car images"));
+    const files = req.files as Express.Multer.File[];
+    const car: ICar = await prisma.car.create({
+      data: {
+        title,
+        description,
+        price: Number(price),
+        userId: req.user.id,
+        categoryId,
+      },
+    });
 
-  if (!req.files) return next(new Error("Kindly upload car images"));
-  const files = req.files as Express.Multer.File[];
-  const car: ICar = await prisma.car.create({
-    data: {
-      title,
-      description,
-      price: Number(price),
-      userId: req.user.id,
-      categoryId,
-    },
-  });
+    await Promise.all(
+      files.map(async (file: any) => {
+        await prisma.image.create({
+          data: {
+            name: file.filename,
+            carId: car.id,
+          },
+        });
+      })
+    );
 
-  await Promise.all(
-    files.map(async (file: any) => {
-      await prisma.image.create({
-        data: {
-          name: file.filename,
-          carId: car.id,
-        },
-      });
-    })
-  );
+    const carWithImages: ICar | null = await prisma.car.findUnique({
+      where: {
+        id: car.id,
+      },
+      include: {
+        images: true,
+      },
+    });
 
-  const carWithImages: ICar | null = await prisma.car.findUnique({
-    where: {
-      id: car.id,
-    },
-    include: {
-      images: true,
-    },
-  });
-
-  res.status(200).json({
-    status: "ok",
-    data: carWithImages,
-  });
+    res.status(200).json({
+      status: "ok",
+      data: carWithImages,
+    });
+  } catch (error: any) {
+    next(new Error(error.message));
+  }
 };
