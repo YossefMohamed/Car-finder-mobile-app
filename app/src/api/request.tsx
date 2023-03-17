3415303af2c71fa4e383d32e4ae201bdd6003517
Yossef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2023-03-17 10:30:01 +0200

    use axios interceptors to refresh token

--- app/src/api/request.tsx
+++ app/src/api/request.tsx
@@ -1,24 +1,68 @@
 import axios, { AxiosError } from "axios";
 
-const client = (() => {
-  return axios.create({
-    baseURL: "http://192.168.0.68:5000/api/",
-  });
-})();
+const client = axios.create({
+  baseURL: "http://192.168.0.68:5000/api/",
+});
 
-const request = async function (options: any, token?: string) {
+const request = async function (
+  options: any,
+  token?: string,
+  useRefreshToken?: (token: string) => Promise<void>,
+  refreshToken?: string
+) {
   let data: any;
-  console.log(options, token);
-  client.interceptors.request.use(function (config) {
-    config.headers.Authorization = token ? `Bearer ${token}` : "";
-    return config;
-  });
+  client.interceptors.request.use(
+    (config) => {
+      console.log(options, token);
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  client.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response.status === 401 && !originalRequest._retry) {
+        try {
+          const token: string = await new Promise((resolve) => {
+            axios
+              .post("http://192.168.0.68:5000/api/refresh", {
+                refreshToken,
+              })
+              .then((response) => {
+                resolve(response.data.token);
+              });
+          });
+          if (useRefreshToken) {
+            await useRefreshToken(token);
+          }
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+          originalRequest._retry = true;
+
+          return client(originalRequest);
+        } catch (error) {
+          return Promise.reject(error);
+        }
+      }
+    }
+  );
   const onSuccess = (response: any) => {
     data = response.data.data;
     return data;
   };
 
   const onError = (error: AxiosError) => {
+    console.log(error.message);
     return Promise.reject(error.response!.data);
   };
 
